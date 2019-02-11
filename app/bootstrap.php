cb30b948d3e3a8608810266fd963ab8356c3abea
Jan Tojnar <jtojnar@gmail.com>
2019-02-11 19:47:00 +0100

    Add CRC 2019 configuration

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -12,6 +12,9 @@ $configurator->setTempDirectory(__DIR__ . '/../temp');
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
+if (file_exists(__DIR__ . '/config/private.neon')) {
+	$configurator->addConfig(__DIR__ . '/config/private.neon');
+}
 
 $container = $configurator->createContainer();
 
