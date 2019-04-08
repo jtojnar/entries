d0febaa3cb0a726633b3462dcc1a70f7a1a211a3
Jan Tojnar <jtojnar@gmail.com>
2019-04-09 00:41:13 +0200

    Add mamut configuration

--- app/config/InvoiceModifier.php
+++ app/config/InvoiceModifier.php
@@ -6,6 +6,7 @@ namespace App;
 
 use App\Model\Invoice;
 use App\Model\Team;
+use App\Presenters\TeamPresenter;
 use Nette;
 
 class InvoiceModifier {
@@ -14,55 +15,23 @@ class InvoiceModifier {
 	public static function modify(Team $team, Invoice $invoice, array $parameters): void {
 		$eventDate = $parameters['eventDate'];
 
-		if ($team->category === 'MJ' || $team->category === 'WJ' || $team->category === 'XJ') {
-			self::adjustJuniorStagePrices($invoice);
+		if ($team->category === 'RD') {
+			$numberOfChildren = \count(array_filter(iterator_to_array($team->persons), function($person) use ($eventDate) {
+				$age = $person->birth->diff($eventDate, true)->y;
+
+				return $age < 15;
+			}));
+
+			$personEntryFeeItemName = TeamPresenter::serializeInvoiceItem([
+				'type' => '~entry',
+				'scope' => 'person',
+			]);
+
+			$items = $invoice->items;
+
+			$items[$personEntryFeeItemName]['amount'] -= $numberOfChildren;
+
+			$invoice->items = $items;
 		}
-
-		$data = $team->getJsonData();
-		if ($data->friday2h === 'yes' && $data->saturday5h === 'yes' && $data->sunday4h === 'yes') {
-			$invoice->addItem('all_stages_discount', -$invoice->items['team:enum:friday2h:yes']['price']);
-		}
-
-		self::fixPersonItemAmounts($invoice, \count($team->persons));
-	}
-
-	private static function adjustJuniorStagePrices(Invoice $invoice): void {
-		$items = $invoice->items;
-
-		if (isset($items['team:enum:friday2h:yes'])) {
-			$items['team:enum:friday2h:yes']['price'] -= 10;
-		}
-
-		if (isset($items['team:enum:saturday5h:yes'])) {
-			$items['team:enum:saturday5h:yes']['price'] -= 20;
-		}
-
-		if (isset($items['team:enum:sunday4h:yes'])) {
-			$items['team:enum:sunday4h:yes']['price'] -= 20;
-		}
-
-		$invoice->items = $items;
-	}
-
-	private static function fixPersonItemAmounts(Invoice $invoice, $personCount): void {
-		$items = $invoice->items;
-
-		if (isset($items['team:enum:friday2h:yes'])) {
-			$items['team:enum:friday2h:yes']['amount'] = $personCount;
-		}
-
-		if (isset($items['team:enum:saturday5h:yes'])) {
-			$items['team:enum:saturday5h:yes']['amount'] = $personCount;
-		}
-
-		if (isset($items['team:enum:sunday4h:yes'])) {
-			$items['team:enum:sunday4h:yes']['amount'] = $personCount;
-		}
-
-		if (isset($items['all_stages_discount'])) {
-			$items['all_stages_discount']['amount'] = $personCount;
-		}
-
-		$invoice->items = $items;
 	}
 }
