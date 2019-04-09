dd696955fc329d7ebceea67c3f5b6ec541f78bef
Jan Tojnar <jtojnar@gmail.com>
2019-04-09 18:32:19 +0200

    .maintenance.php -> 503.phtml
    https://github.com/dg/sandbox/commit/f2acaa7cb28d3768aeb43fe4730838738f733d4

--- www/index.php
+++ www/index.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Uncomment this line if you must temporarily take down your site for maintenance.
-// require '.maintenance.php';
-
 App\Booting::boot()
 	->createContainer()
 	->getByType(Nette\Application\Application::class)
