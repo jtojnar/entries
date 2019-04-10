7f59782e32e0552e1002a7599bb414a05f842cdb
Jan Tojnar <jtojnar@gmail.com>
2019-04-11 00:22:49 +0200

    Include .htaccess in bundle

--- utils/package.sh
+++ utils/package.sh
@@ -8,6 +8,6 @@ git clean -f
 composer install --no-dev --optimize-autoloader --prefer-dist
 yarn install
 yarn run build
-zip -r $ENTRIES_ZIPBALL app log temp vendor www install.sql readme.md -x 'www/assets' '*/.git*' '*/tests*' '*/tools*' '*/examples*' '*/license*' '*/LICENSE*' '*/CODE_OF_CONDUCT.md' '*/readme.md' '*/contributing.md' '*/.travis.yml' '*/.appveyor.yml' '*/composer.json' '*/composer.lock' '*/package.json' '*/bower.json' '*/yarn.lock'
+zip -r $ENTRIES_ZIPBALL app log temp vendor www install.sql readme.md .htaccess -x 'www/assets' '*/.git*' '*/tests*' '*/tools*' '*/examples*' '*/license*' '*/LICENSE*' '*/CODE_OF_CONDUCT.md' '*/readme.md' '*/contributing.md' '*/.travis.yml' '*/.appveyor.yml' '*/composer.json' '*/composer.lock' '*/package.json' '*/bower.json' '*/yarn.lock'
 
 sed -i "s/ENTRIES_VERSION/$ENTRIES_VERSION/g;s/ENTRIES_ZIPBALL/$ENTRIES_ZIPBALL/g" utils/bintray.json
