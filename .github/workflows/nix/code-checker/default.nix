5f1e0f899a85b72b102e68e65fb1faaf31c83171
Jan Tojnar <jtojnar@gmail.com>
2021-06-02 19:34:01 +0200

    flake: update

--- .github/workflows/nix/code-checker/default.nix
+++ .github/workflows/nix/code-checker/default.nix
@@ -4,7 +4,7 @@
 
 let
   composerEnv = import ./composer-env.nix {
-    inherit (pkgs) stdenv writeTextFile fetchurl php unzip phpPackages;
+    inherit (pkgs) stdenv lib writeTextFile fetchurl php unzip phpPackages;
   };
 in
 import ./php-packages.nix {
--- .github/workflows/nix/code-checker/php-packages.nix
+++ .github/workflows/nix/code-checker/php-packages.nix
@@ -7,7 +7,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "latte-latte-fc94bd63fe995b40cb219109026e76f281c709c2";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/latte/zipball/fc94bd63fe995b40cb219109026e76f281c709c2;
+          url = "https://api.github.com/repos/nette/latte/zipball/fc94bd63fe995b40cb219109026e76f281c709c2";
           sha256 = "11hsh3j6ah9r3l3fmh9zzlghcxhpg7whl4i6x6rwxp98vz477qcp";
         };
       };
@@ -17,7 +17,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "nette-application-783ad6fc6444f63314175131885c04b3dd0291dd";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/application/zipball/783ad6fc6444f63314175131885c04b3dd0291dd;
+          url = "https://api.github.com/repos/nette/application/zipball/783ad6fc6444f63314175131885c04b3dd0291dd";
           sha256 = "1lw8cnd8pb8c4mns3ryl1xlxx9frvj6dxg2v2lzah3dyx7v9p320";
         };
       };
@@ -27,7 +27,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "nette-caching-b9ecbf920f240bd1ab14900d9a77876924ad7fb4";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/caching/zipball/b9ecbf920f240bd1ab14900d9a77876924ad7fb4;
+          url = "https://api.github.com/repos/nette/caching/zipball/b9ecbf920f240bd1ab14900d9a77876924ad7fb4";
           sha256 = "1diissvhm2v0wzgc4v5gggsafrl7d71irdpdqg2hhm1k2rqxw3ky";
         };
       };
@@ -37,7 +37,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "nette-code-checker-faae7ac04dcdebf319f1f69c128ff8015f930d11";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/code-checker/zipball/faae7ac04dcdebf319f1f69c128ff8015f930d11;
+          url = "https://api.github.com/repos/nette/code-checker/zipball/faae7ac04dcdebf319f1f69c128ff8015f930d11";
           sha256 = "1yi1aay2iikiyr7bpp0mwjn2a8ac7rnrmfy7sbqr5qgk6mv9ymwn";
         };
       };
@@ -47,7 +47,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "nette-command-line-7027cbee2d283b5d482d11350dbb5399cc33b745";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/command-line/zipball/7027cbee2d283b5d482d11350dbb5399cc33b745;
+          url = "https://api.github.com/repos/nette/command-line/zipball/7027cbee2d283b5d482d11350dbb5399cc33b745";
           sha256 = "178mamjz1kv6kpdpdzwss96sgkflzi9byv9ahhw80vhgrlldb1fk";
         };
       };
@@ -57,7 +57,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "nette-component-model-66409cf5507c77edb46ffa88cf6a92ff58395601";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/component-model/zipball/66409cf5507c77edb46ffa88cf6a92ff58395601;
+          url = "https://api.github.com/repos/nette/component-model/zipball/66409cf5507c77edb46ffa88cf6a92ff58395601";
           sha256 = "0pf2xkwfsy6amcbl8mdks7awgnqlmvc3adk8hz9s2lfjmgh4zfc2";
         };
       };
@@ -67,7 +67,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "nette-finder-4ad2c298eb8c687dd0e74ae84206a4186eeaed50";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/finder/zipball/4ad2c298eb8c687dd0e74ae84206a4186eeaed50;
+          url = "https://api.github.com/repos/nette/finder/zipball/4ad2c298eb8c687dd0e74ae84206a4186eeaed50";
           sha256 = "1bsgpmlk3mvyv3x5i6q4f6mrd2dpcjjb691z6gax71mim2ip7alb";
         };
       };
@@ -77,7 +77,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "nette-forms-ecb5f7b8c82585c5fc4698ccb6815542fe6b2db4";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/forms/zipball/ecb5f7b8c82585c5fc4698ccb6815542fe6b2db4;
+          url = "https://api.github.com/repos/nette/forms/zipball/ecb5f7b8c82585c5fc4698ccb6815542fe6b2db4";
           sha256 = "18cms2s0shwfjfz8r99r6dpx9pn6fs9vdkql5i2dcra313yagmkl";
         };
       };
@@ -87,7 +87,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "nette-http-e4d8d360c66c8af9512ca13ab629d312af2b3ce3";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/http/zipball/e4d8d360c66c8af9512ca13ab629d312af2b3ce3;
+          url = "https://api.github.com/repos/nette/http/zipball/e4d8d360c66c8af9512ca13ab629d312af2b3ce3";
           sha256 = "07wlpzfdfqp2wkmz84avnfqkbfxl45cgxxw27yaxq1naca9zpyxr";
         };
       };
@@ -97,7 +97,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "nette-neon-a5b3a60833d2ef55283a82d0c30b45d136b29e75";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/neon/zipball/a5b3a60833d2ef55283a82d0c30b45d136b29e75;
+          url = "https://api.github.com/repos/nette/neon/zipball/a5b3a60833d2ef55283a82d0c30b45d136b29e75";
           sha256 = "0hx4vqg0khfrg5ww6dzw3dnwqnnnfzq7rapl15xws22m5jkl2w21";
         };
       };
@@ -107,7 +107,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "nette-routing-603c697f3df7ed214795d4e8e8c58fbf981232b1";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/routing/zipball/603c697f3df7ed214795d4e8e8c58fbf981232b1;
+          url = "https://api.github.com/repos/nette/routing/zipball/603c697f3df7ed214795d4e8e8c58fbf981232b1";
           sha256 = "04pz69hznr3gwfql5qnqds5ldgnvw8ywyl2rw1g83qdyg1rnvzry";
         };
       };
@@ -117,7 +117,7 @@ let
       src = composerEnv.buildZipPackage {
         name = "nette-utils-c09937fbb24987b2a41c6022ebe84f4f1b8eec0f";
         src = fetchurl {
-          url = https://api.github.com/repos/nette/utils/zipball/c09937fbb24987b2a41c6022ebe84f4f1b8eec0f;
+          url = "https://api.github.com/repos/nette/utils/zipball/c09937fbb24987b2a41c6022ebe84f4f1b8eec0f";
           sha256 = "1ik3xfqzwrxdbrjpxlfpkk83k1l98xjvyc3hsc6472dpkmm96n0l";
         };
       };
