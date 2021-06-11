e32393275b09fc7b3a03664b5f89b2d8aee32abf
Jan Tojnar <jtojnar@gmail.com>
2021-06-11 16:41:32 +0200

    assets: Skip empty popovers on list page
    Popover craps out otherwise.

--- www/assets/js/list.js
+++ www/assets/js/list.js
@@ -1,12 +1,14 @@
 import Popover from 'bootstrap/js/dist/popover';
 
 export function register() {
-	document.querySelectorAll('[data-bs-content]').forEach(element =>
-		new Popover(element, {
-			placement: 'left',
-			html: true,
-			trigger: 'hover',
-			container: 'body',
-		})
-	);
+	document.querySelectorAll('[data-bs-content]').forEach(element => {
+		if (element.getAttribute('data-bs-content') !== '') {
+			new Popover(element, {
+				placement: 'left',
+				html: true,
+				trigger: 'hover',
+				container: 'body',
+			});
+		}
+	});
 };
