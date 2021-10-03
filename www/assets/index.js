8e64a2254f551c1e9eea0830004ae37d5a21997d
Jan Tojnar <jtojnar@gmail.com>
2021-10-03 07:28:35 +0200

    list: Fix hiding filter submit button
    Additionally, this removes JavaScript from HTML attributes, making us ready for strict CSP.

--- www/assets/index.js
+++ www/assets/index.js
@@ -3,13 +3,17 @@ import './css/style.scss';
 
 // Application
 import netteForms from 'nette-forms';
+import * as changeFormSubmit from './js/change-form-submit';
 import * as form from './js/form';
 import * as formWarnings from './js/form-warnings';
 import * as icons from './js/icons';
 import * as list from './js/list';
+import * as noscript from './js/noscript';
 
 netteForms.initOnLoad();
+changeFormSubmit.register();
 form.register();
 formWarnings.register();
 icons.register();
 list.register();
+noscript.register();
