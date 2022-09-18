5308f110beda3823950bd8e73993ad577eead15f
Jan Tojnar <jtojnar@gmail.com>
2022-09-18 23:55:24 +0200

    assets: update dependencies
    Notably, includes a bump to a stable version of CodeMirror 6.
    
    Also needed to update bootstrap code:
    https://github.com/twbs/bootstrap/commit/9f099d3e4f6fe5b0c5454d2462cc22d06b6f4a1b
    https://github.com/twbs/bootstrap/commit/de0dfca9a1749990319cdfcbb7f1584df09d7091

--- www/assets/js/editor.js
+++ www/assets/js/editor.js
@@ -1,20 +1,20 @@
-import { EditorState, EditorView, basicSetup } from '@codemirror/basic-setup';
+import { EditorView, basicSetup } from 'codemirror';
 import { html } from '@codemirror/lang-html';
 
 function editorFromTextArea(textarea, extensions) {
 	let view = new EditorView({
-		state: EditorState.create({
-			doc: textarea.value,
-			extensions,
-		}),
+		doc: textarea.value,
+		extensions,
 	});
 
 	textarea.parentNode.insertBefore(view.dom, textarea);
 	textarea.style.display = 'none';
 
-	if (textarea.form) textarea.form.addEventListener('submit', () => {
-		textarea.value = view.state.doc.toString();
-	});
+	if (textarea.form) {
+		textarea.form.addEventListener('submit', () => {
+			textarea.value = view.state.doc.toString();
+		});
+	}
 
 	return view;
 }
