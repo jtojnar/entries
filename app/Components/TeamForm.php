jtojnar/entries

commit 9b8e1ed7d274f07260ad18b86076bcf3c92ce303

Do not anchor TeamForm in constructor
This will make it easier to test in isolation.

app/Components/TeamForm.php
@@ -11,7 +11,6 @@ use App\Model\Configuration\Fields\Field;
 use App\Model\InputModifier;
 use Contributte\Translation\Wrappers\NotTranslate;
 use Nette\Application\UI;
-use Nette\ComponentModel\IContainer;
 use Nette\Forms\Container;
 use Nette\Forms\Controls;
 use Nette\Utils\Json;
@@ -21,15 +20,14 @@ use Nette\Utils\Json;
  */
 final class TeamForm extends UI\Form {
 	public function __construct(
-		/** @var array<string, string> */
+		/** @var string[] */
 		private readonly array $countries,
 		/** @var array<string, int> */
 		private readonly array $reservationStats,
 		private readonly Entries $entries,
-		IContainer $parent = null,
-		string $name = null,
+		private readonly bool $canModifyLocked,
 	) {
-		parent::__construct($parent, $name);
+		parent::__construct();
 	}
 
 	public function onRender(): void {
@@ -91,8 +89,6 @@ final class TeamForm extends UI\Form {
 			}
 
 			$isDisabled = $field->disabled ?? false;
-			$presenter = $this->getPresenter();
-			\assert($presenter !== null);
 			if ($field instanceof Fields\CheckboxlistField || $field instanceof Fields\EnumField) {
 				\assert($input instanceof BootstrapCheckboxList || $input instanceof BootstrapRadioList);
 				$options = $field instanceof Fields\EnumField ? $field->options : $field->items;
@@ -111,7 +107,7 @@ final class TeamForm extends UI\Form {
 						return $itemDisabled;
 					},
 				);
-				if (!$presenter->getUser()->isInRole('admin')) {
+				if (!$this->canModifyLocked) {
 					$input->setDisabled($disabledFields);
 				} else {
 					$input->getItemLabelPrototype()->{'data-is-visually-disabled?'} = $disabledFields;
@@ -124,7 +120,7 @@ final class TeamForm extends UI\Form {
 					$isDisabled = $isDisabled || $numberReserved >= $limit;
 				}
 
-				if (!$presenter->getUser()->isInRole('admin')) {
+				if (!$this->canModifyLocked) {
 					$input->setDisabled($isDisabled);
 				} else {
 					$input->getControlPrototype()->setAttribute('data-is-visually-disabled', $isDisabled);

app/Forms/TeamFormFactory.php
@@ -11,7 +11,6 @@ use App\Model\Configuration\Entries;
 use Contributte\Translation\Wrappers\Message;
 use Kdyby\Replicator\Container as ReplicatorContainer;
 use Nette;
-use Nette\ComponentModel\IContainer;
 use Nette\Forms\Container;
 use Nette\Forms\Controls\SubmitButton;
 use Nette\Localization\Translator;
@@ -28,20 +27,19 @@ final class TeamFormFactory {
 	}
 
 	/**
+	 * @param string[] $countries
 	 * @param array<string, int> $reservationStats
 	 */
 	public function create(
 		array $countries,
 		array $reservationStats = [],
-		IContainer $parent = null,
-		string $name = null,
+		bool $canModifyLocked = false,
 	): TeamForm {
 		$form = new TeamForm(
-			$countries,
-			$reservationStats,
-			$this->entries,
-			$parent,
-			$name,
+			countries: $countries,
+			reservationStats: $reservationStats,
+			entries: $this->entries,
+			canModifyLocked: $canModifyLocked,
 		);
 
 		$form->setTranslator($this->translator);

app/Presenters/TeamPresenter.php
@@ -290,10 +290,9 @@ final class TeamPresenter extends BasePresenter {
 		}
 
 		$form = $this->teamFormFactory->create(
-			$this->countries->fetchIdNamePairs(),
-			$reservationStats,
-			$this,
-			$name,
+			countries: $this->countries->fetchIdNamePairs(),
+			reservationStats: $reservationStats,
+			canModifyLocked: $this->getUser()->isInRole('admin'),
 		);
 		/** @var \Nette\Forms\Controls\SubmitButton */
 		$save = $form['save'];

commit 723f04a036107c95a932e77fd0fd36a8e40da0b8

TeamForm: Fix category constraint validation
This got broken by https://github.com/jtojnar/entries/commit/9b8e1ed7d274f07260ad18b86076bcf3c92ce303

app/Components/TeamForm.php
@@ -28,6 +28,8 @@ final class TeamForm extends UI\Form {
 		private readonly bool $canModifyLocked,
 	) {
 		parent::__construct();
+
+		$this->onValidate[] = $this->checkCategoryConstraints(...);
 	}
 
 	public function onRender(): void {
@@ -200,6 +202,27 @@ final class TeamForm extends UI\Form {
 			return $default;
 		} else {
 			return $field->default ?? null;
+		}
+	}
+
+	private function checkCategoryConstraints(self $form, \stdClass $data): void {
+		// If submitter is `true`, no specific submit button was pressed but let’s check the form.
+		// Since `onValidate` is only called on form submission, it cannot be `false` but we will use `is_bool` to satisfy PHPStan.
+		$validationScope = \is_bool($form->isSubmitted()) ? null : $form->isSubmitted()->getValidationScope();
+		if ($validationScope === []) {
+			// onValidate event is called even when submit button’s validation scope is empty.
+			// But we do not want to validate when adding/removing team members, which sets the scope to empty array.
+			return;
+		}
+
+		$categoryField = $form->getComponent('category');
+		$constraints = $this->entries->categories->allCategories[$data->category]->constraints;
+		/** @var iterable<iterable<string, mixed>> */
+		$persons = $data->persons;
+		foreach ($constraints as $constraint) {
+			if (!$constraint->admits($persons)) {
+				$categoryField->addError($constraint->getErrorMessage());
+			}
 		}
 	}
 }

app/Forms/TeamFormFactory.php
@@ -62,17 +62,6 @@ final class TeamFormFactory {
 		$category->setRequired();
 		$form['category'] = $category;
 
-		if ($category->value !== null) {
-			$constraints = $this->entries->categories->allCategories[$category->value]->constraints;
-			foreach ($constraints as $constraint) {
-				$rule = $category->addCondition(true); // not to block the export of rules to JS
-				$rule->addRule(
-					static fn(CategoryEntry $entry): bool => $constraint->admits($entry->getForm()?->getUnsafeValues(null)['persons']),
-					$constraint->getErrorMessage(),
-				);
-			}
-		}
-
 		$rule = $category->addCondition(true); // not to block the export of rules to JS
 		$rule->addRule(function(CategoryEntry $entry) use ($defaultMaxMembers): bool {
 			$category = $entry->getValue();

utils/phpstan.neon
@@ -17,3 +17,5 @@ parameters:
 		- '(Readonly property App\\Model\\Configuration\\CategoryData::\$allCategories is already assigned.)'
 		# https://github.com/phpstan/phpstan/issues/8954
 		- '(Method App\\Model\\Configuration\\Helpers::ensureSubclassOf\(\) should return class-string<V>\|null but returns class-string<U>.)'
+		# https://github.com/phpstan/phpstan-nette/issues/82
+		- '(Property Nette\\Forms\\Container::\$onValidate \(array<callable\(App\\Components\\TeamForm, mixed\): void>\) does not accept non-empty-array<\(callable\(App\\Components\\TeamForm, mixed\): void\)|\(Closure\(App\\Components\\TeamForm, stdClass\): void\)>.)'