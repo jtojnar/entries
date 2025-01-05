8ec31fdee8bb8e505ee992ef214aed9b24c8401a
Jan Tojnar <jtojnar@gmail.com>
2025-01-06 00:16:29 +0100

    wip: Configuration: Fix currency fallback
    Previously it would prefer fallback currency

--- app/Model/Configuration/Fees.php
+++ app/Model/Configuration/Fees.php
@@ -14,8 +14,21 @@ final readonly class Fees {
 	) {
 	}
 
+	/**
+	 * @param array<mixed> $fees
+	 */
+	public static function fromRoot(array $fees): self {
+		$currencyCode = Helpers::ensureNonEmptyString('fees.currency', $fees['currency'] ?? 'CZK');
+		$currency = new Currency($currencyCode);
+
+		return new self(
+			currency: $currency,
+			person: isset($fees['person']) ? new Money($fees['person'] * 100, $currency) : null,
+		);
+	}
+
 	public static function from(array $fees, ?self $parentFees = null): self {
-		$currency = $parentFees?->currency ?? new Currency($fees['currency'] ?? 'CZK');
+		$currency = isset($fees['currency']) ? new Currency($fees['currency']) : $parentFees?->currency;
 
 		return new self(
 			currency: $currency,
