ec08542e310c7ff27ebcc4395eb5d6d1f123dd69
Jan Tojnar <jtojnar@gmail.com>
2025-01-30 20:25:38 +0100

    config/CustomInputModifier: Factor out country ID into constant
    Avoid magic numbers.

--- app/Config/CustomInputModifier.php
+++ app/Config/CustomInputModifier.php
@@ -14,6 +14,8 @@ use Nette\Forms\Form;
  * @param callable(BaseControl): (BaseControl|Rules) $whenNotPlaceholder
  */
 final class CustomInputModifier implements InputModifier {
+	private const COUNTRY_ID = 46; // Czechia
+
 	public static function modify(Control $input, IContainer $container, callable $whenNotPlaceholder): void {
 		// we also have some inputs that are based on Nextras\FormComponents\Fragments\UIComponent\BaseControl
 		if ($input instanceof BaseControl && $input->getName() === 'registry_address') {
@@ -22,9 +24,9 @@ final class CustomInputModifier implements InputModifier {
 
 			/** @var BaseControl */
 			$country = $container->getComponent('country');
-			$country->addCondition(Form::NOT_EQUAL, 46)->toggle($pairId, false);
+			$country->addCondition(Form::NOT_EQUAL, self::COUNTRY_ID)->toggle($pairId, false);
 			$input->setRequired(false);
-			$whenNotPlaceholder($input)->addConditionOn($country, Form::EQUAL, 46)->setRequired();
+			$whenNotPlaceholder($input)->addConditionOn($country, Form::EQUAL, self::COUNTRY_ID)->setRequired();
 		}
 	}
 }
