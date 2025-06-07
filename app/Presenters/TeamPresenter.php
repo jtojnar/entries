jtojnar/entries

commit fefa7ac17b298a73be6bca5105fca263a7f7ca7d

Fix some PHPStan strict rules issues

app/Presenters/TeamPresenter.php
@@ -120,7 +120,7 @@ final class TeamPresenter extends BasePresenter {
 
 	public function renderRegister(): void {
 		$form = $this->getComponent('teamForm');
-		if (!$form->isSubmitted()) {
+		if ($form->isSubmitted() === false) {
 			// Create sufficient number of person subforms for the most common team size (when it is greater than minimum team size).
 			$remainingMembers = $this->entries->initialMembers - $this->entries->minMembers;
 			/** @var ReplicatorContainer */
@@ -156,7 +156,7 @@ final class TeamPresenter extends BasePresenter {
 			}
 
 			$form = $this->getComponent('teamForm');
-			if (!$form->isSubmitted()) {
+			if ($form->isSubmitted() === false) {
 				$default = [];
 				$default['name'] = $team->name;
 				$default['category'] = $team->category;
@@ -250,7 +250,7 @@ final class TeamPresenter extends BasePresenter {
 		$teamFields = $this->entries->teamFields;
 		$personFields = $this->entries->personFields;
 
-		if (\count($teams)) {
+		if (\count($teams) > 0) {
 			if ($type === 'meos') {
 				$exporter = new Exporters\MeosExporter($teams, $this->categoryFormatter);
 				$this->response->setContentType($exporter->getMimeType(), 'UTF-8');
@@ -758,11 +758,11 @@ final class TeamPresenter extends BasePresenter {
 	private function filterRedir(Nette\Forms\Form $form): void {
 		$parameters = [];
 
-		if ($this->request->getQuery('category')) {
+		if ($this->request->getQuery('category') !== null) {
 			$parameters['category'] = $this->request->getQuery('category');
 		}
 
-		if ($this->request->getQuery('status')) {
+		if ($this->request->getQuery('status') !== null) {
 			$parameters['status'] = $this->request->getQuery('status');
 		}
 

app/Components/SportidentControl.php
@@ -66,7 +66,7 @@ final class SportidentControl extends BaseControl {
 
 				$this->neededControl->addCondition(Form::EQUAL, true)->toggle($this->cardIdControl->htmlId . '-warning', false);
 
-				$this->cardIdControl->getControlPrototype()->{'data-form-warning-rules'} = Helpers::exportRules($rules);
+				$this->cardIdControl->getControlPrototype()->setAttribute('data-form-warning-rules', Helpers::exportRules($rules));
 			}
 		});
 

app/Components/TeamForm.php
@@ -110,7 +110,7 @@ final class TeamForm extends UI\Form {
 				if (!$this->canModifyLocked) {
 					$input->setDisabled($disabledFields);
 				} else {
-					$input->getItemLabelPrototype()->{'data-is-visually-disabled?'} = $disabledFields;
+					$input->getItemLabelPrototype()->setAttribute('data-is-visually-disabled?', $disabledFields);
 				}
 			} else {
 				$limitName = $field instanceof Fields\LimitableField ? $field->getLimitName() : null;
@@ -128,7 +128,7 @@ final class TeamForm extends UI\Form {
 			}
 
 			if (isset($field->applicableCategories)) {
-				$input->getControlPrototype()->{'data-applicable-categories'} = Json::encode($field->applicableCategories);
+				$input->getControlPrototype()->setAttribute('data-applicable-categories', Json::encode($field->applicableCategories));
 			}
 
 			/** @var ?class-string<InputModifier> */
@@ -181,7 +181,7 @@ final class TeamForm extends UI\Form {
 			return array_reduce(
 				$field->options,
 				static function(?string $carry, Fields\Item $item): ?string {
-					if ($item->default) {
+					if ($item->default === true) {
 						return $item->name;
 					}
 
@@ -192,7 +192,7 @@ final class TeamForm extends UI\Form {
 			$default = [];
 
 			foreach ($field->items as $item) {
-				if ($item->default) {
+				if ($item->default === true) {
 					$default[] = $item->name;
 				}
 			}

app/Helpers/Iter.php
@@ -71,7 +71,8 @@ final class Iter {
 	 */
 	public static function last(iterable $iterable): mixed {
 		$value = null;
-		foreach ($iterable as $value) {
+		foreach ($iterable as $item) {
+			$value = $item;
 		}
 
 		return $value;

app/Model/Configuration/Category.php
@@ -86,7 +86,7 @@ final class Category {
 
 	private static function parseConstraints(array $constraints, DateTimeInterface $eventDate): array {
 		return array_map(function(string $constraint) use ($eventDate): Constraints\Constraint {
-			if (preg_match(self::CONSTRAINT_REGEX, $constraint, $match)) {
+			if (preg_match(self::CONSTRAINT_REGEX, $constraint, $match) === 1) {
 				['quant' => $quant, 'key' => $key, 'op' => $op, 'val' => $val] = $match;
 				$quant = self::QUANT_LOOKUP[$quant];
 
@@ -116,10 +116,8 @@ final class Category {
 						$op,
 						$comparedValue,
 					);
-				} else {
-					throw new \PHPStan\ShouldNotHappenException();
 				}
-			} elseif (preg_match(self::AGGREGATE_CONSTRAINT_REGEX, $constraint, $match)) {
+			} elseif (preg_match(self::AGGREGATE_CONSTRAINT_REGEX, $constraint, $match) === 1) {
 				['aggr' => $aggr, 'key' => $key, 'op' => $op, 'val' => $val] = $match;
 				$aggr = self::AGGR_LOOKUP[$aggr];
 				$op = self::OP_LOOKUP[$op];

app/Presenters/ErrorPresenter.php
@@ -36,7 +36,7 @@ final class ErrorPresenter implements Nette\Application\IPresenter {
 		$this->logger->log($exception, ILogger::EXCEPTION);
 
 		return new Responses\CallbackResponse(function(Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
-			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
+			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type')) === 1) {
 				require __DIR__ . '/../Templates/Error/500.phtml';
 			}
 		});

app/Presenters/InvoicePresenter.php
@@ -79,11 +79,11 @@ final class InvoicePresenter extends BasePresenter {
 		[$scope, $type, $key, $value] = array_merge(explode(':', $item), ['', '', '', '']);
 
 		if ($scope === 'person' && $type === '~entry') {
-			return (string) $this->translator->translate('messages.billing.invoice.fees.person');
+			return $this->translator->translate('messages.billing.invoice.fees.person');
 		}
 
-		$fields = $scope == 'team' ? 'teamFields' : 'personFields';
-		$field = $this->entries->$fields[$key] ?? null;
+		$fields = $scope == 'team' ? $this->entries->teamFields : $this->entries->personFields;
+		$field = $fields[$key] ?? null;
 
 		if ($field !== null && $field->label !== null) {
 			$label = $this->translator->translate($field->label);
@@ -92,14 +92,14 @@ final class InvoicePresenter extends BasePresenter {
 		}
 
 		if ($field instanceof Fields\SportidentField) {
-			return (string) $this->translator->translate('messages.billing.invoice.fees.si');
+			return $this->translator->translate('messages.billing.invoice.fees.si');
 		}
 
-		if ($field instanceof Fields\EnumField && !empty($value) && ($option = $field->options[$value] ?? null)?->label !== null) {
+		if ($field instanceof Fields\EnumField && $value !== '' && ($option = $field->options[$value] ?? null)?->label !== null) {
 			return $label . ' ' . $this->translator->translate($option->label);
 		}
 
-		if ($field instanceof Fields\CheckboxlistField && !empty($value) && ($option = $field->items[$value] ?? null)?->label !== null) {
+		if ($field instanceof Fields\CheckboxlistField && $value !== '' && ($option = $field->items[$value] ?? null)?->label !== null) {
 			return $label . ' ' . $this->translator->translate($option->label);
 		}
 		if ($field instanceof Fields\CheckboxField) {

commit 2439d8435d30be8601ae353fd619d6c8968a195d

Teams/lists: Fix category filter with All
All has empty string as a value.

This regressed in fefa7ac17b298a73be6bca5105fca263a7f7ca7d

app/Presenters/TeamPresenter.php
@@ -786,12 +786,14 @@ final class TeamPresenter extends BasePresenter {
 	private function filterRedir(Nette\Forms\Form $form): void {
 		$parameters = [];
 
-		if ($this->request->getQuery('category') !== null) {
-			$parameters['category'] = $this->request->getQuery('category');
+		$category = $this->request->getQuery('category');
+		if ($category !== null && $category !== '') {
+			$parameters['category'] = $category;
 		}
 
-		if ($this->request->getQuery('status') !== null) {
-			$parameters['status'] = $this->request->getQuery('status');
+		$status = $this->request->getQuery('status');
+		if ($status !== null && $status !== '') {
+			$parameters['status'] = $status;
 		}
 
 		if (\count($parameters) === 0) {